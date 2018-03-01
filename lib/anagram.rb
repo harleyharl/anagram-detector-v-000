94345cc1451135c2fbd551a814cd1e0a082f08fc
Harley Rayner <harleymteden@gmail.com>
2018-03-01 04:10:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,10 @@ class Anagram
     @word = word
   end
 
-  def match(word_list)
+  def match(possible_anagrams)
+    word_letters = word.split("").sort
+    possible_anagrams.each do |anagram| anagram.split("").sort
+    end 
   end
 
 
